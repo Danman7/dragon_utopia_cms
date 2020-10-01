ec16f50a59f3593f1312863e739ee2c1adb2bd79
Danail Atanasov <danail.atanasov@moneycorp.com>
2020-10-01 18:22:12 +0300

    chore: setup cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'dz1clzhcm',
+      api_key: '128827822299811',
+      api_secret: 'iWS6T2BC-XFQlbpRXNz_3oNz89E',
+    },
+  },
+})
